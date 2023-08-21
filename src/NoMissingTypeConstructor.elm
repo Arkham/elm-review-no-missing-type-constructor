fd61ce520d72c62cb7c4026888b1f878c68295b6
Ju Liu <liuju86@gmail.com>
2023-08-21 17:38:26 +0100

    Update elm-review and elm-syntax to latest

--- src/NoMissingTypeConstructor.elm
+++ src/NoMissingTypeConstructor.elm
@@ -65,6 +65,7 @@ rule =
             , foldProjectContexts = foldProjectContexts
             }
         |> Rule.withContextFromImportedModules
+        |> Rule.providesFixesForProjectRule
         |> Rule.fromProjectRuleSchema
 
 
